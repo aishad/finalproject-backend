72b4773865f50d7ba2a39dfe8eb5e970e9974d6b
aishad <aishadiallo@gmail.com>
2018-05-17 13:18:06 -0400

    new

--- api.js
+++ api.js
@@ -1,67 +1,63 @@
-const express = require('express')
-const app = express()
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
-var uri = "mongodb+srv://aisha:1111@cluster0-cbiyv.mongodb.net/test?retryWrites=true";
+app.use(bodyParser.raw({ type: "*/*", limit: "50mb" }));
 
-    // SOME FUNCTIONS
-    // insert data into listings
+app.use(express.static("images"));
 
-    app.post('/createListing', (req, res) => {
-        let reqb = JSON.parse(req.reqb.toString());
-        // reqb = {
-        //     artistName: "aisha",
-        //     name: 'print',
-        //     price: 100,
-        //     cat: "Prints",
-        //     blurb: "cool print",
-        //     quantity: 1,
-        //     imageURL1: '/items/aisha.jpg',
-        //     imageURL2: '/items/pillow.jpg',
-        //     imageURL3: '/items/embroidery.jpg',
-        // }
+app.post("/createListing", (req, res) => {
+  //let reqb = JSON.parse(req.body.toString());
+  // reqb = {
+  //     artistName: "aisha",
+  //     name: 'print',
+  //     price: 100,
+  //     cat: "Prints",
+  //     blurb: "cool print",
+  //     quantity: 1,
+  //     imageURL1: '/items/aisha.jpg',
+  //     imageURL2: '/items/pillow.jpg',
+  //     imageURL3: '/items/embroidery.jpg',
+  // }
 
-        let RESB = {itemID: "123"};
-        res.send(JSON.stringify(RESB))
-    })
-    
-    app.get('/getartistdetails', (req,res) =>{
-        //let reqb = JSON.parse(req.body.toString());
-        reqb = {artistName : "caro"}
+  let RESB = { itemID: "123" };
+  res.send(JSON.stringify(RESB));
+});
 
-        RESB = {
-            artistName: "caro",
-            bio: "I'm a cool artist",
-            location: "Montreal, Canada",
-            imageURL: "mypic.jpg",
-            items: [
-              {
-                itemID: "123457",
-                name: "Awesome Embroidery",
-                price: 100,
-                artistName: "caro",
-                imageURL: "embroidery.jpg",
-                cat: "Spring",
-                blurb: "Best embroidery ever!",
-                quantity: 1
-              },
-              {
-                itemID: "123458",
-                name: "Pillow",
-                price: 100,
-                artistName: "caro",
-                imageURL: "pillow.jpg",
-                cat: "Popular",
-                blurb: "Best pillow ever!",
-                quantity: 2
-              }
-            ]         
-        }
-        res.send(JSON.stringify(RESB))
-    })
+app.get("/getartistdetails", (req, res) => {
+  //let reqb = JSON.parse(req.body.toString());
+  reqb = { artistName: "caro" };
 
-    app.get()
+  RESB = {
+    artistName: "caro",
+    bio: "I'm a cool artist",
+    location: "Montreal, Canada",
+    imageURL: "mypic.jpg",
+    items: [
+      {
+        itemID: "123457",
+        name: "Awesome Embroidery",
+        price: 100,
+        artistName: "caro",
+        imageURL: "embroidery.jpg",
+        cat: "Spring",
+        blurb: "Best embroidery ever!",
+        quantity: 1
+      },
+      {
+        itemID: "123458",
+        name: "Pillow",
+        price: 100,
+        artistName: "caro",
+        imageURL: "pillow.jpg",
+        cat: "Popular",
+        blurb: "Best pillow ever!",
+        quantity: 2
+      }
+    ]
+  };
+  res.send(JSON.stringify(RESB));
+});
 
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(4000, () => console.log("Listening on port 4000!"));
